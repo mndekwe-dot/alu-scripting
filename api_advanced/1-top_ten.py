54b5e8f935b67bafe8b53624d5fd7b8e980b3f54
mndekwe-dot <m.ndekwe@alustudent.com>
2026-02-25 21:25:32 +0200

    Refactor top_ten function for improved clarity

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-""" 1-top_ten.py """
+"""Module to query Reddit API and print top 10 hot post titles."""
 import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    """Print the titles of the first 10 hot posts for a given subreddit.
+
+    Prints None if the subreddit is invalid.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "python:subreddit.top.ten:v1.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
         return
-    posts = response.json()['data']['children']
+    posts = response.json().get("data", {}).get("children", [])
     for post in posts:
-        print(post['data']['title'])
+        print(post.get("data", {}).get("title"))
