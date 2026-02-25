e3d31d5707c47b0c47819aaf226fb59afcaba01e
mndekwe-dot <m.ndekwe@alustudent.com>
2026-02-25 21:32:02 +0200

    Refactor top_ten function for improved readability
    Refactor top_ten function to simplify response handling and title printing.

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,32 +1,16 @@
 #!/usr/bin/python3
-"""Print titles of the first 10 hot posts for a subreddit."""
-
+""" 1-top_ten.py """
 import requests
 
 
 def top_ten(subreddit):
-    """Query Reddit API and print first 10 hot post titles."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    params = {"limit": 10}
-
-    try:
-        response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            allow_redirects=False,
-        )
-    except requests.RequestException:
-        print(None)
-        return
-
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
         return
-
-    posts = response.json().get("data", {}).get("children", [])
-    for post in posts[:10]:
-        title = post.get("data", {}).get("title")
-        if title is not None:
-            print(title)
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
